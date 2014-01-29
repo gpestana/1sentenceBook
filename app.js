02b613f2a83b331fbc4e648bed236f5d35e927cc
Pestana Gonçalo <g6pestana@gmail.com>
2014-01-29 23:56:34 +0200

    structure refactoring

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express');
-var routes = require('routes');
+var routes = require('./app/routes');
 var http = require('http');
 var path = require('path');
 
@@ -12,14 +12,14 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/app/views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(router);
+app.use(routes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
