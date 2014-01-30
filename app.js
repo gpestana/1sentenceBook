ba31a8e15853e7d9891f51b95c19cb68cf2cb58d
Pestana Gonçalo <g6pestana@gmail.com>
2014-01-30 03:37:07 +0200

    aws amazon search draft included

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./app/routes');
-var http = require('http');
-var path = require('path');
 
-var app = express();
+ var express = require('express');
+ var http = require('http');
+ var routes = require('./app/routes');
+ var path = require('path');
+
+ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -19,21 +20,28 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(routes);
+app.use(routes)
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 
+/*
+ * Routing table
+ */
+
 app.get('/', routes.index);
-app.get('/test', routes.test);
+app.get('/test/:var', routes.test);
 app.get('*', routes.error);
 
 
+
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
+
+
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,34 @@
 
 /*
- * Main Routing table
+ * Routing handlers
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+ var aws = require("aws-lib");
 
-exports.test = function(req, res){
-  res.render('test', { title: 'Test motherfucker!!' });
-};
+ //export values from a .gitignore file
+ var sk = "--"
+ var ak = "--"
+ var tag = "--"
 
-exports.error = function(req, res){
-	res.render('error', {title: 'Ups..'});
-}; 
+ exports.index = function(req, res) {
+
+ 	var prodAdv = aws.createProdAdvClient(ak, sk, tag);
+
+ 	var options = {SearchIndex: "Books", Keywords: "Javascript"}
+
+ 	prodAdv.call("ItemSearch", options, function(err, result) {
+ 		console.log(result);
+ 	})
+
+ 	res.render('index', { title: 'Express' });
+ };
+
+ exports.test = function(req, res){
+ 	res.render('test', { 
+ 		title: 'Test motherfucker!!',
+ 		param: req.params.var });
+ };
+
+ exports.error = function(req, res){
+ 	res.render('error', {title: 'Ups..'});
+ }; 
