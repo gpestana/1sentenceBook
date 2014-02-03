76ab7ca4c095b697caf3966c12da0962235672fe
Pestana Gonçalo <g6pestana@gmail.com>
2014-02-03 20:33:57 +0200

    lib

---
+++ app/lib/search.js
@@ -0,0 +1,21 @@
+ var aws = require("aws-lib");
+
+ /* export values from a file marked as .gitignore --> Create your own with 
+ your credentials */
+ var aws_credentials = require("./../../private_credentials") 
+ var sk = aws_credentials.getSerialKey();
+ var ak = aws_credentials.getActivationKey();
+ var tag = aws_credentials.getTag();
+
+
+ exports.search = function(title) {
+
+ 	var prodAdv = aws.createProdAdvClient(ak, sk, tag);
+ 	var options = {SearchIndex: "Books", Title: title, IdType: "ISBN"}
+
+ 	prodAdv.call("ItemSearch", options, function(err, result) {
+ 		console.log("Results for '"+ title+"'");
+ 		console.log(result);
+ 		return result;
+ 	});
+ }
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,15 +3,8 @@
  * Routing handlers
  */
 
- var aws = require("aws-lib");
 
- /* export values from a file marked as .gitignore --> Create your own with 
- your credentials */
- var aws_credentials = require("./../../private_credentials") 
- var sk = aws_credentials.getSerialKey();
- var ak = aws_credentials.getActivationKey();
- var tag = aws_credentials.getTag();
-
+ search = require('./../lib/search')
 
 
  exports.index = function(req, res) {
@@ -22,30 +15,37 @@
 
  exports.search = function(req, res) {
 
- 	var prodAdv = aws.createProdAdvClient(ak, sk, tag);
- 	var options = {SearchIndex: "Books", Title: req.params.title, IdType: "ISBN"}
+ 	var title = req.params.title; 
 
- 	prodAdv.call("ItemSearch", options, function(err, result) {
- 		console.log("Results for '"+ req.params.title+"'");
- 		console.log(result);
+ 	res.render('search', result = callback(title), { 
+ 		title: title,
+ 		results: JSON.stringify(result["Items"]["Item"])}); 	
 
- 		res.render('search', { 
- 		title: req.params.title,
- 		results: JSON.stringify(result["Items"]["Item"])});
- 	});
-
- }
-
-
-
- exports.test = function(req, res){
- 	res.render('test', { 
- 		title: 'Test motherfucker!!',
- 		param: req.params.var });
  };
 
 
+ function callback(title) {
 
- exports.error = function(req, res){
- 	res.render('error', {title: 'Ups..'});
- }; 
+ 	return search.search(title);
+
+ 	//console.log("CALLBACK");
+/*
+ 	res.render('search', { 
+ 		title: title,
+ 		results: JSON.stringify(result["Items"]["Item"])});
+ 
+*/
+};
+
+
+exports.test = function(req, res){
+	res.render('test', { 
+		title: 'Test motherfucker!!',
+		param: req.params.var });
+};
+
+
+
+exports.error = function(req, res){
+	res.render('error', {title: 'Ups..'});
+}; 
